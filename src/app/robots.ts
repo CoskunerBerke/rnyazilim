18fef135321201057984e7357faa333a10f7ec3a
CoskunerBerke <coskunerberke@gmail.com>
2026-07-19 23:04:22 +0300

    feat: configure Next.js static HTML export and update robots/sitemap dynamic parameters

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { companyInfo } from "@/content/company";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = `https://${companyInfo.domain}`;
   
